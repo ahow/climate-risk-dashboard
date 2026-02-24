9c56c300adc35ef3e8788b651afc7763f273d397
ahow <andyhoward@me.com>
2026-02-24 22:48:08 +0000

    Sync Climate Risk Dashboard from Replit

--- server/index.ts
+++ server/index.ts
@@ -3,6 +3,7 @@ import { registerRoutes } from "./routes";
 import { serveStatic } from "./static";
 import { createServer } from "http";
 import { ensureSchemaUpdates } from "./db";
+import { recoverOrphanedOperations } from "./services/operationManager";
 
 const app = express();
 const httpServer = createServer(app);
@@ -100,6 +101,7 @@ app.use((req, res, next) => {
     },
     () => {
       log(`serving on port ${port}`);
+      setTimeout(() => recoverOrphanedOperations(), 3000);
     },
   );
 })();
